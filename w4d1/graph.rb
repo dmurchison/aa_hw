18acbfd64172a5e0fcf1bbb9957f50087474cfb5
Duncan Murchison <duncancmurchison@gmail.com>
2022-04-18 23:19:09 -0400

    w4d1

---
+++ w4d1/graph.rb
@@ -0,0 +1,23 @@
+class Graph
+
+  def initialize
+  end
+
+  def bfs(starting_node, target)
+  end
+
+  def dfs(starting_node, target)
+  end
+
+end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
